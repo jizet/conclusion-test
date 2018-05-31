fd30b74a385c7e80b4125adf0822f25492db99a4
Jose <jose.zuleta@governance.com>
2018-05-31 15:59:28 +0200

    Change logic to make code more readeable

--- src/App/App.component.js
+++ src/App/App.component.js
@@ -9,30 +9,22 @@ import './App.style.css';
 
 class App extends Component {
 
-
-
   componentDidMount() {
     this.props.getData()
   }
 
   render() {
-    if (process.env.NODE_ENV !== 'production') {
-      whyDidYouUpdate(React);
-    }
-    if (this.props.loading) {
-      return (
-        <div className="container text-center default-padding">Loading...</div>
-      );
-    }
     return (
-      <div className="App">
-        <header className="container default-padding">
-          <UserManagement />
-        </header>
-        <main className="main default-padding">
-          <UserList />
-        </main>
-      </div>
+      this.props.loading 
+        ? <div className="container text-center default-padding">Loading...</div>
+        : <div className="App">
+            <header className="container default-padding">
+              <UserManagement />
+            </header>
+            <main className="main default-padding">
+              <UserList />
+            </main>
+          </div>
     );
   }
 }
